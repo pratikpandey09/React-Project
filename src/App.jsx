aa89fa5129b4f580dd7a14986cef4b38389a4232
pratikpandey09 <pratikp8354@gmail.com>
2025-06-17 21:57:21 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,65 @@
-import { useState } from 'react'
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import { useState } from 'react'
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// // import App from './App';
+// import Home from './components/Home/Home';
+// import About from './components/About/About';
+// import Contact from './components/Contact/Contact';
+// import Github from './components/Github/Github';
+// import User from './components/User/User';
+// import Header from './components/Header/Header'
+// import Footer from './components/Footer/Footer'
+
+
+
+// // const root = ReactDOM.createRoot(document.getElementById('root'));
+
+    
+// // root.render(
+// //   <BrowserRouter>
+// //     <Routes>
+// //         <Route path='/' element={<Home />} />
+// //         <Route path='/about' element={<About />} />
+// //         <Route path='/contact' element={<Contact />} />
+// //         <Route path='/github' element={<Github />} />
+// //         <Route path='/user/:userid' element={<User />} />
+// //     </Routes>
+// //   </BrowserRouter>
+// // )
+
+// // function App() {
+// //   return (
+// //     <>
+// //     <Header/>
+// //     {/* <Outlet /> */}
+// //     <Footer />
+// //     </>
+// //   )
+// // }
+
+// // ✅ This is the part you must include:
+// export default App;
+
+
+import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
-import Home from './components/Home/Home';
-import About from './components/About/About';
-import Contact from './components/Contact/Contact';
-import Github from './components/Github/Github';
-import User from './components/User/User';
+import Home from "./components/Home/Home"
+import About from "./components/About/About"
+import Header from "./components/Header/Header"
+import Footer from "./components//Footer/Footer"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
     <Routes>
-        <Route path='/' index element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/github' element={<Github />} />
-        <Route path='/user' element={<User />} />
+      <Route path='/' element={<Home />} />
+      <Route path="/about" element={<About />} />
     </Routes>
-  </BrowserRouter>
-);
+    <Footer />
+    </BrowserRouter>
+  )
+}
 
-export default App
+
--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,43 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+
+
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+// import { Route, RouterProvider, BrowserRouter, createBrowserRouter, createRoutesFromElements, Router } from 'react-router-dom'
+// import Home from './components/Home/Home.jsx'
+// import About from './components/About/About.jsx'
+// import Contact from './components/Contact/Contact.jsx'
+// import User from './components/User/User.jsx'
+// import Github, { githubInfoLoader } from './components/Github/Github.jsx'
+
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path='/' element={<App />}>
+//       <Route path='' element={<Home />} />
+//       <Route path='about' element={<About />} />
+//       <Route path='contact' element={<Contact />} />
+//       <Route path='user/:userid' element={<User />} />
+//       <Route 
+//       loader={githubInfoLoader}
+//       path='github' 
+//       element={<Github />}
+//        />
+//     </Route>
+//   )
+// )
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//     {/* <App/> */}
+//       <RouterProvider router={router} />
+//     </BrowserRouter>
+//   </React.StrictMode>  
+// )
