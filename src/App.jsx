04e4ad7f9b9696d00d083440e57fda113dacea81
pratikpandey09 <pratikp8354@gmail.com>
2025-06-17 21:55:24 +0530

    push to vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import { useState } from 'react'
-import { BrowserRouter, Outlet} from "react-router";
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
 import Home from './components/Home/Home';
-import { Link } from "react-router";
-import { useLocation } from 'react-router-dom';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Github from './components/Github/Github';
+import User from './components/User/User';
 
-
-function App() {
-  const darkMode=(e)=>{
-  
-  }
-
-  return (
-    <div>
-   <Header/>
-   <Outlet/>
-   <Footer/>
-    </div>
-  )
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Routes>
+        <Route path='/' index element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/github' element={<Github />} />
+        <Route path='/user' element={<User />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App
